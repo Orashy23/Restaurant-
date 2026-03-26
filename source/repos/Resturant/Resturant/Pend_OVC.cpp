897c07d38a761851b7205dc6071a923f66d4d6b8
Orashy23 <omarorashy8@gmail.com>
2026-03-26 04:19:17 +0200

    Pend_OVC

--- source/repos/Resturant/Resturant/Pend_OVC.cpp
+++
@@ -1,8 +0,0 @@
-#include "Pend_OVC.h"
-
-void Cancel_Order()
-{
-
-
-
-}
--- source/repos/Resturant/Resturant/Pend_OVC.h
+++ source/repos/Resturant/Resturant/Pend_OVC.h
@@ -1,5 +1,38 @@
 #pragma once
 #include "LinkedQueue.h"
 #include "Order.h"
+class Pend_OVC : public LinkedQueue<Order*> 
+{
 
-void Cancel_Order();
+public:
+
+	bool Cancel_Order( int cancelledID , Order* &cancelledOrder ) {
+
+		LinkedQueue<Order*> tempQueue;
+		Order* tempOrder;
+		bool found = false;
+		
+		while (this->dequeue(tempOrder)) {
+		
+			if (tempOrder->getID() == cancelledID) {
+
+				found = true;
+				tempOrder = cancelledOrder;
+			}
+			else {
+
+				tempQueue.enqueue(tempOrder);
+
+			}
+		}
+
+		while (tempQueue.dequeue(tempOrder)) {
+
+			this->enqueue(tempOrder);
+		}
+
+
+		return found;
+	}
+
+};

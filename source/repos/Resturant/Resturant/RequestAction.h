18b6443ba6a72561968fc6f38c94e32ad428e423
Moahmed Gharbawy <mohamed.gharbawy1610@gmail.com>
2026-04-01 09:32:33 +0200

    Requestaction.h

--- source/repos/Resturant/Resturant/RequestAction.h
+++ source/repos/Resturant/Resturant/RequestAction.h
@@ -1,72 +1,49 @@
-//#pragma once
-//#include "Order.h"
-//#include "Chef.h"
-//#include "Scooter.h"
-//#include "LinkedQueue.h"
-//#include "priQueue.h"
-//#include "Action.h"
-//
-//
-//class RequestAction : public Action
-//{
-//private:
-//// to be wridden from the file;
-//	string type;
-//	int ID;
-//	int size;
-//	int price;
-//
-////Dine in only
-//	int duration;
-//	bool canShare;
-//	int seats;
-//// Delevery only
-//	int distance;
-//public:
-//	RequestAction(int t,string typ,int sz ,int prc, int dur, int dist , bool share, int seatC ,int id) : Action(t)
-//	{
-//
-//		type = typ;
-//		ID = id;
-//		size = sz;
-//		price = prc;
-//		duration = dur;
-//		canShare = share;
-//		seats = seatC;
-//		distance = dist;
-//	}
-//	void act(Restaurant* pRes);
-//	bool isDineIn() { return type == "DineIn"; }
-//	bool isTakeAway() { return type == "TakeAway"; }
-//	void setType(string t) { type = t; }
-//	void setDineInInfo(int dur, bool share, int seatC);
-//	void setDistance(int d) { distance = d; }
-//	~RequestAction() {};
-//};
-//
-//
-//// /// //// //// //// ////////// CPP //////////////////
-//
-//
-//void RequestAction::act(Restaurant* pRes)
-//{
-//	Order* order = new Order(ID, type, price, size, getTime());
-//	order->setDineInInfo(seats, duration, canShare);
-//	order->setDistance(distance);
-//	pRes->addOrder(order);
-//	if(type == "DineIn")
-//		pRes->addDineInOrder(order);
-//	
-//	else if(type == "TakeAway")
-//		pRes->addTakeAwayOrder(order);
-//
-//
-//}
-//
-//
-//
-//
-//
-//
-//
-// //  TO be completed by Gharbawy
+#pragma once
+#include "Action.h"
+#include "Restaurant.h"
+#include "Order.h"
+#include <string>
+
+class RequestAction : public Action
+{
+private:
+    int ID;
+    string type;
+    int size;
+    int price;
+//dine in
+    int seats, duration;
+    bool canShare;
+
+ //delivery
+    int distance;
+
+public:
+    
+    RequestAction(int time, string typ, int id, int sz, int prc, int sts, int dur, bool shr, int dist)
+        : Action(time), type(typ), ID(id), size(sz), price(prc), seats(sts), duration(dur), canShare(shr), distance(dist)
+    {
+    }
+
+  
+    virtual void act(Restaurant* pRes) 
+    {
+        //new order
+        Order* pOrd = new Order(ID, type, price, size, getTime());
+
+        
+        if (type == "ODG" || type == "ODN")
+        {
+            pOrd->setDineInInfo(seats, duration, canShare);
+        }
+        else if (type == "OVC" || type == "OVG" || type == "OVN")
+        {
+            pOrd->setDistance(distance);
+        }
+//resturant control.
+        pRes->AddtoPendingList(pOrd);
+    }
+
+    // Virtual destructor for safe memory cleanup 
+    virtual ~RequestAction() {}
+};

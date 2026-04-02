6eb422e6ebedeea0252c728d12eb89b9ae545978
yichen-7 <yseif922@gmail.com>
2026-04-02 21:00:32 +0200

    Fit table

--- source/repos/Resturant/Resturant/Fit_Tables.h
+++ source/repos/Resturant/Resturant/Fit_Tables.h
@@ -2,16 +2,59 @@
 #include "priQueue.h"
 #include "Table.h"
 
-// Derived class to support best-fit selection
+
 class Fit_Tables : public priQueue<Table*>
 {
 public:
     Fit_Tables() {}
+    void addTable(Table* t) { 
+        enqueue(t, -t->getFreeSeats());
 
-    // Returns the best table that fits the required seats
-    Table* getBest(int requiredSeats)
-    {
-        // TO DO: Implement best-fit dequeue logic
-        return nullptr;
+
     }
+    Table* getBest(int requiredSeats) {
+        priQueue<Table*> temp;
+        Table* best = nullptr;
+        Table* t;
+        int p;
+
+        while (dequeue(t, p)) {
+            if (t->getFreeSeats() >= requiredSeats && best == nullptr) {
+                best = t;
+            }
+            else {
+                temp.enqueue(t, p);
+            }
+        }
+
+        while (temp.dequeue(t, p)) {
+            enqueue(t, p);
+        }
+		return best;
+	}
+
+
+    void print() const {
+        priNode<Table*>* current = head;
+        while (current != nullptr) {
+            int p;
+            Table* t = current->getItem(p);
+            cout << "[T" << t->getID() << ", "<< t->getCapacity() << ", "<< t->getFreeSeats() << "] ";
+            current = current->getNext();
+        }
+        cout << endl;
+    }
+    }
+    int getcount() const { 
+        priNode<Table*>* current = head;
+		int count = 0;
+        while(current) {
+            count++;
+            current = current->getNext();
+		}
+		return count;
+    }
+
+    
+    
 };

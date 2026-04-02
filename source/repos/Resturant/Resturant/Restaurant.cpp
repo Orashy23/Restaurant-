680880721961cb21490b146b5bb6c10c711bc356
Moahmed Gharbawy <mohamed.gharbawy1610@gmail.com>
2026-04-02 22:48:49 +0200

    ~~~

--- source/repos/Resturant/Resturant/Restaurant.cpp
+++ source/repos/Resturant/Resturant/Restaurant.cpp
@@ -75,38 +75,19 @@ void Restaurant::CancelOrder(int id)
 
 Restaurant::~Restaurant()
 {
-	Order* pOrd;
-	int pri;
-	while (Pend_ODN.dequeue(pOrd))
-		delete pOrd;
-	while (Pend_ODG.dequeue(pOrd))
-		delete pOrd;
-	while (Pend_OT.dequeue(pOrd))
-		delete pOrd;
-	while (Pend_OVN.dequeue(pOrd))
-		delete pOrd;
-	while (Pend_OVC_List.dequeue(pOrd))
-		delete pOrd;
-	while (Pend_OVG.dequeue(pOrd, pri))
-		delete pOrd;
-	while (Cooking_Orders.dequeue(pOrd,pri))
-		delete pOrd;
-	while (Ready_OV_List.dequeue(pOrd))
-		delete pOrd;
-	while (Cancelled_orders.dequeue(pOrd))
-		delete pOrd;
-	while (Finished_Orders.pop(pOrd))
-		delete pOrd;
-    while(Cooking_Orders.dequeue(pOrd , pri ))
-		delete pOrd;
-	while (Ready_OV_List.dequeue(pOrd))
-		delete pOrd;
-	while (RDY_OD.dequeue(pOrd))
-		delete pOrd;
-	while (RDY_OT.dequeue(pOrd))
-		delete pOrd;
-	while (Cancelled_orders.dequeue(pOrd))
-		delete pOrd;
-	while (Finished_Orders.pop(pOrd))
-		delete pOrd;
+    Order* pOrd;
+    int pri;
+    while (Pend_ODN.dequeue(pOrd))    delete pOrd;
+    while (Pend_ODG.dequeue(pOrd))    delete pOrd;
+    while (Pend_OT.dequeue(pOrd))     delete pOrd;
+    while (Pend_OVN.dequeue(pOrd))    delete pOrd;
+    while (Pend_OVC_List.dequeue(pOrd)) delete pOrd;
+    while (Pend_OVG.dequeue(pOrd, pri)) delete pOrd;
+    while (Cooking_Orders.dequeue(pOrd, pri)) delete pOrd;
+    while (Ready_OV_List.dequeue(pOrd)) delete pOrd;
+    while (RDY_OD.dequeue(pOrd))      delete pOrd;
+    while (RDY_OT.dequeue(pOrd))      delete pOrd;
+    while (Cancelled_orders.dequeue(pOrd)) delete pOrd;
+    while (Finished_Orders.pop(pOrd)) delete pOrd;
+    while (InServ_Orders.dequeue(pOrd, pri)) delete pOrd;
 }

50e2c0b3a2c366951fa7e648dfc3c8a4639b73e4
Orashy23 <omarorashy8@gmail.com>
2026-04-02 19:35:52 +0200

    Res.ccp 1st function

--- source/repos/Resturant/Resturant/Restaurant.cpp
+++ source/repos/Resturant/Resturant/Restaurant.cpp
@@ -1,2 +1,54 @@
 #include "Restaurant.h"
 #include "Order.h"
+
+Restaurant::Restaurant()
+{}
+
+void Restaurant::AddtoPendingList(Order* pOrd)
+{
+	string type = pOrd->getType();
+	if (type == "ODN") {
+
+		PENDODN.enqueue(pOrd);
+
+	}
+	
+	else if (type == "ODG") {
+		
+        PENDODG.enqueue(pOrd);
+	
+    }
+    else if (type == "OT")
+    {
+    
+        Pend_OT.enqueue(pOrd);
+    
+    }
+    else if (type == "OVN")
+    {
+    
+        Pend_OVN.enqueue(pOrd);
+    
+    }
+    else if (type == "OVC")
+    {
+    
+        Pend_OVC_List.enqueue(pOrd);
+    
+    }
+    else if (type == "OVG")
+    {
+
+
+
+    }
+
+void Restaurant::ExecuteActions(int currentTimestep)
+{
+
+
+
+}
+
+
+Restaurant::~Restaurant();

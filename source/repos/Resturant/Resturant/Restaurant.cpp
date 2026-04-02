6c6c5ff23da558a11c3350a02f0d9f715476d9db
Moahmed Gharbawy <mohamed.gharbawy1610@gmail.com>
2026-04-02 20:58:45 +0200

    resturant des

--- source/repos/Resturant/Resturant/Restaurant.cpp
+++ source/repos/Resturant/Resturant/Restaurant.cpp
@@ -88,4 +88,16 @@ Restaurant::~Restaurant()
 		delete pOrd;
 	while (Finished_Orders.pop(pOrd))
 		delete pOrd;
+    while(Cooking_Orders.dequeue(pOrd))
+		delete pOrd;
+	while (Ready_OV_List.dequeue(pOrd))
+		delete pOrd;
+	while (RDY_OD.dequeue(pOrd))
+		delete pOrd;
+	while (RDY_OT.dequeue(pOrd))
+		delete pOrd;
+	while (Cancelled_orders.dequeue(pOrd))
+		delete pOrd;
+	while (Finished_Orders.pop(pOrd))
+		delete pOrd;
 }
